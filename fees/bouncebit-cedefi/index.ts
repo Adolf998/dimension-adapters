99da54704e25928d60683c6b176e4c2f9fd36b23
bill <>
2024-12-13 15:38:49 +0800

    update bouncebit cedefi fee url

--- fees/bouncebit-cedefi/index.ts
+++ fees/bouncebit-cedefi/index.ts
@@ -3,7 +3,7 @@ import { CHAIN } from "../../helpers/chains";
 import fetchURL from "../../utils/fetchURL";
 import { getUniqStartOfTodayTimestamp } from '../../helpers/getUniSubgraphFees';
 
-const bbscanApiURL = "https://api.bbscan.io/api/fee/stats";
+const ApiURL = "https://internal.bouncebitapi.com/api/fee/stats";
 
 interface DailyStats {
   date: string;
@@ -13,13 +13,14 @@ interface DailyStats {
 
 const fetchBounceBitCedefiStats = async (timestamp: any) => {
   const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000));
-  const stats: DailyStats[] = (await fetchURL(bbscanApiURL)).result;
+  const stats: DailyStats[] = (await fetchURL(ApiURL)).result;
 
   const dailyFees = (()=> {
     const idx = stats.findIndex(stat => stat.timestamp === dayTimestamp);
     if (idx === -1) return 0;
     if (idx === 0) return stats[0]?.fee || 0;
-    return (stats[idx]?.fee || 0) - (stats[idx - 1]?.fee || 0)
+    const _fees = (stats[idx]?.fee || 0) - (stats[idx - 1]?.fee || 0)
+    return _fees > 0 ? _fees : 0
   })();
 
   return {
